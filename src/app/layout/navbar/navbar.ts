be82dc61f50251fa4c975284568f7ed333c0291a
Mohammad El-Hassan <mohammadelhsn@gmail.com>
2025-08-19 10:48:41 -0400

    Hmm

--- src/app/layout/navbar/navbar.ts
+++ src/app/layout/navbar/navbar.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { Router, RouterModule } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, MatIcon],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
